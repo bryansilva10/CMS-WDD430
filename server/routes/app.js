1ca82a9de4c11af218e766ebd2d29f06ceff1933
Bryan Guillen <bryanguillen@Bryans-MacBook-Pro.local>
2020-06-26 10:49:03 -0600

    Week 10-11

---
+++ server/routes/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+// const path = require('path');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  // res.render('index', { title: 'CMS' });
+  res.sendFile(path.join(__dirname, 'dist/cms/index.html'));
+});
+
+module.exports = router;
